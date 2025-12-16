1140be58b3a3615cca1ed1d3db9e533e131d151b
jickjack00 <jarkhammond@outlook.com>
2025-12-16 16:16:08 +0000

    general improvements and slight functionality
    added general visual improvements, as well as robustness and added some functionality, though IT HAS NOT BEEN TESTED YET

--- customer/login/index.php
+++ customer/login/index.php
@@ -1,12 +1,43 @@
 <?php
-    // someone deleted the navbar and config files; waiting for a fix. for now, commented
-    include __DIR__ . '/../../database_code/config.php';
-    include __DIR__ . '/../../components/navbar.php'; 
-    session_start();
-    $registered = false; // test var - replace with own logic
-    $error = ''; // test var - replace with own logic
+    require_once '../../database_code/config.php';
+    require_once '../../components/navbar.php';
+    // session_start();
+    // uncomment if the navbar ever removes it
 
-    // no functionality yet
+    $error = '';
+    // above var is a test; logic will be in place soon
+
+    // was user sent here from register page?
+    $registered = isset($_GET['registered']);
+
+    if (isset($_POST['login'])) {
+        // set the entered data to a variable for ease of referral
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        // look up the username
+        $sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
+        $result = mysqli_query($conn, $sql);
+
+        // if any matches, proceed
+        if ($result && mysqli_num_rows($result) === 1) {
+            $user = mysqli_fetch_assoc($result);
+
+            // check password via password_verify
+            if (password_verify($password, $user['password'])) {
+                // if successful, log in
+                $_SESSION['logged_in'] = true;
+                $_SESSION['username'] = $user['username'];
+                header("Location: index.php");
+                exit;
+            } else {
+                $error = "Incorrect password.";
+            }
+        } else {
+            $error = "User not found.";
+        }
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,11 +47,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Customer Login</title>
     <!-- <link rel="stylesheet" href="../../styles/styles.css"> -->
-    <!-- stylesheet ruins the page as is. doesn't need to be complicated, as it's a login page, it'll be ok -->
+    <!-- stylesheet messes with a lot of elements, preferably do not refer to it -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" 
+    integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <style>
+        /* if needed can implement this into styles.css but the stylesheet messes with a lot of content on this page, take caution */
         body { /* this is for the gradient background, but can be changed to fit another element */
             /* tone down the colour brightness here to give more attention to the brighter elements */
-            background-image: linear-gradient(to bottom left, rgb(0, 70, 0), rgb(120, 0, 0));
+            background-image: linear-gradient(to bottom left, rgb(120, 0, 0), rgb(0, 70, 0), rgb(120, 0, 0));
             background-repeat: no-repeat;
             background-attachment: fixed;
             background-size: 100% 100%;
@@ -31,18 +65,18 @@
         }
         
         .content-container { /* has everything on the page excluding navbar and footer. those can be edited in their respective files ofc... */
-            margin-left: 15%;
-            margin-right: 15%;
+            margin-left: 10%;
+            margin-right: 10%;
             z-index: 2;
         }
 
         .yellow-border-box { /* general content container, but for sections rather than the whole page */
             border: 2px solid yellow;
             border-radius: 10px;
-            /* to not conflict stuff with the border, put padding as seen below */
+            padding: 1%;
             padding-left: 10%;
             padding-right: 5%;
-            /* brighten up colours to attract attention. previous colours are literally just darker versions of default grn/red */
+            /* brighten up colours to attract attention */
             background-image: linear-gradient(to bottom left, red, green, red);
             background-repeat: no-repeat;
             background-attachment: fixed;
@@ -50,14 +84,17 @@
         }
 
         input {
-            background-color: skyblue;
             border-radius: 5px;
+            width: 30%
         }
 
         .submit-button {
-            background-color: skyblue;
+            /* via box shadow, added 3D-like effect */
+            color: black;
+            background-color: white;
             padding: 5px;
-            border: groove;
+            padding-left: 20px;
+            padding-right: 20px;
             border-radius: 5px;
             transition-duration: 0.4s;
             cursor: pointer;
@@ -65,30 +102,32 @@
         }
 
         .submit-button:hover {
+            /* change colour when hovered */
             color: black;
-            background-color: white;
+            background-color: skyblue;
         }
 
         .submit-button:active {
+            /* animate 3D effect when clicked, plus change colour for further visual feedback */
             color: black;
-            background-color: ;
+            background-color: rgb(100, 150, 180);
             box-shadow: 0 1px #666;
             transform: translateY(4px);
         }
-        
-        footer { /* not sure why but end points are visible on the footer. do not touch these values, and leave this on this page only */
-            margin-left: -9px;
-            margin-right: -4px;
+
+        .form-group label {
+            /* affects the label text, but not the input text itself */
+            color: white
         }
     </style>
 </head>
+<!-- compensate for navbar and footer -->
 <br><br>
 <body style="font-family: 'Arial'">
-    <div class="snow">
     <div class="content-container">
         <div class="yellow-border-box">
-            <h1 style="font-size: 36px">Log In</h1>
-            <h3>Hurry! Log in by <?php echo date("d/m/Y", strtotime("+1 week")); ?> to claim 50 free Roulette spins!</h3>
+            <h1 style="font-size: 36px"><b>Log In</b></h1>
+            <h3><u>Hurry!</u> Log in by <b><?php echo date("d/m/Y", strtotime("+1 week")); ?></b> to claim 50 free Roulette spins!</h3>
 
             <?php if ($registered) { ?>
                 <!-- if the user was referred from the registration page, then say they succeeded and tell them to log in -->
@@ -96,38 +135,35 @@
             <?php } ?>
 
             <?php if ($error !== "") { ?>
-                <!-- if user runs into an error, echo it. add error implementation at the top php -->
+                <!-- if user runs into an error, echo it -->
                 <p><?php echo $error; ?></p>
             <?php } ?>
 
-            <form method="post" action="index.php">
-                <p style="font-size: 18px;">
+            <form method="POST" action="index.php">
+                <div style="font-size: 18px;" class="form-group">
                     <label>Username:</label><br>
                     <!-- inputs can use some styling, basic as-is -->
-                    <input type="text" name="username" required>
-                </p>
-                <p style="font-size: 18px;">
+                    <input type="text" name="username" placeholder=" John Doe" required>
+                </div>
+                <div style="font-size: 18px;" class="form-group">
+                    <label>Email Address:</label><br>
+                    <input type="email" name="email" placeholder=" JohnDoe@email.com" required>
+                </div>
+                <div style="font-size: 18px;" class="form-group">
                     <label>Password:</label><br>
                     <input type="password" name="password" required>
-                </p>
-                <p style="font-size: 18px;">
-                    <label>Email Address:</label><br>
-                    <input type="email" name="email" required>
-                </p>
-                <p>
+                </div>
+                <div class="form-group">
                     <button type="submit" name="login" class="submit-button">Log in</button>
-                </p>
+                </div>
             </form>
 
-            <p>No account yet? <a style="display: inline-block" href="<?php __DIR__ . '/../../customer/register/index.php'?>">Register here</a>.</p>
+            <p>No account yet? <a style="display: inline-block" href="/addictive-app-project/customer/register/index.php">Register here</a>.</p>
         </div>
     </div>
-    </div>
-    <footer>
-        <br><br><br>
-        <?php
-            include __DIR__ . '/../../components/footer.php';
-        ?>
-    </footer>
+    <br><br><br><br>
+    <?php
+        require_once '../../components/footer.php';
+    ?>
 </body>
 </html>
