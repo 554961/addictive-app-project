6f80911f234ced2fb126488691f0d4dd641b3750
Jack <jarkhammond@outlook.com>
2025-12-17 10:27:04 +0000

    functionality finish
    still some bugs, but mainly up to navbar developer at this point

--- customer/login/index.php
+++ customer/login/index.php
@@ -1,41 +1,78 @@
 <?php
     require_once '../../database_code/config.php';
     require_once '../../components/navbar.php';
-    // session_start();
-    // uncomment if the navbar ever removes it
 
+    // bug with this; it doesn't know what "loggedIn" means until the error appears
+    // other than this, all of this php works fine
+    // though this is an issue with the navbar too
+    if ($_SESSION["loggedIn"]) {
+        // user is already signed in; does not need to visit log in page
+        header("location: ../../index.php");
+        // if you're a dev wondering why you get booted back to the index page every time;
+        // DELETE YOUR COOKIES then refresh
+    }
+
+    // instantiate error var for later
     $error = '';
-    // above var is a test; logic will be in place soon
 
     // was user sent here from register page?
     $registered = isset($_GET['registered']);
-
-    if (isset($_POST['login'])) {
-        // set the entered data to a variable for ease of referral
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-
-        // look up the username
-        $sql = "SELECT * FROM users WHERE username = '$username' LIMIT 1";
-        $result = mysqli_query($conn, $sql);
-
-        // if any matches, proceed
+    
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+    
+        // there's probably a much easier way to do this but i don't know it and it works
+        $passwordQry = "SELECT password_hash FROM users WHERE username = '$username'";
+        // in the below 2 lines we're looking for if the entered username and email match any profile
+        $usernameQry = "SELECT username FROM users WHERE username = '$username'";
+        $emailQry = "SELECT email FROM users WHERE username = '$username'";
+        // and because it works, it shouldn't be touched until we guarantee another way...
+    
+        $result = mysqli_query($connection, $usernameQry);
+        // if the inputted username matches an entry...
         if ($result && mysqli_num_rows($result) === 1) {
-            $user = mysqli_fetch_assoc($result);
-
-            // check password via password_verify
-            if (password_verify($password, $user['password'])) {
-                // if successful, log in
-                $_SESSION['logged_in'] = true;
-                $_SESSION['username'] = $user['username'];
-                header("Location: index.php");
-                exit;
+            $result = mysqli_query($connection, $emailQry); // not sure why, but this line prevents an error from appearing
+            // it doesn't affect processing i don't think, but keep it cuz it improves UX
+            $row = mysqli_fetch_assoc($result);
+            // ...AND if the inputted email matches the same entry and the inputted username...
+            if ($row['email'] == $email) {
+                $result = mysqli_query($connection, $passwordQry);
+                $row = mysqli_fetch_assoc($result);
+                
+                // then check both forms of password
+                // if password isn't hashed
+                if ($row["password_hash"] == $password)
+                {
+                    // correct password
+                    echo "Correct password; redirecting...";
+                    session_start();
+            
+                    $_SESSION["loggedIn"] = true;
+                    $_SESSION["username"] = $username;
+                    header("location: ../../index.php?loggedIn=SUCCESS");
+                } 
+                else $error = "Incorrect password.";
+                        
+                
+                // otherwise, check for hashed version
+                if (password_verify($password, $row["password_hash"]))
+                {
+                    // correct hashed password
+                    echo "Correct password; redirecting...";
+                    $_SESSION["loggedIn"] = true;
+                    $_SESSION["username"] = $username;
+                    header("location: ../../index.php?loggedIn=SUCCESS");
+                }
+                // below errors appear just below the "hurry" line of text after the user attempts a sign-in
+                else $error = "Incorrect password.";
             } else {
-                $error = "Incorrect password.";
+                $error = "Incorrect email address.";
             }
         } else {
-            $error = "User not found.";
+            $error = "Incorrect username.";
         }
     }
 ?>
@@ -50,18 +87,24 @@
     <!-- stylesheet messes with a lot of elements, preferably do not refer to it -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" 
     integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!-- this one is fine though -->
     <style>
         /* if needed can implement this into styles.css but the stylesheet messes with a lot of content on this page, take caution */
         body { /* this is for the gradient background, but can be changed to fit another element */
-            /* tone down the colour brightness here to give more attention to the brighter elements */
+            /* toned down the colour brightness here to give more attention to the brighter elements */
             background-image: linear-gradient(to bottom left, rgb(120, 0, 0), rgb(0, 70, 0), rgb(120, 0, 0));
             background-repeat: no-repeat;
             background-attachment: fixed;
             background-size: 100% 100%;
         }
 
-        h1, h3, p {
+        h1, h3, p, div {
             color: white;
+        }
+
+        button {
+            /* literally just for the login button on the navbar */
+            color: black;
         }
         
         .content-container { /* has everything on the page excluding navbar and footer. those can be edited in their respective files ofc... */
@@ -84,8 +127,16 @@
         }
 
         input {
+            /* affects the input text only */
             border-radius: 5px;
-            width: 30%
+            width: 30%;
+            padding-left: 8px;
+            color: black
+        }
+        
+        .form-group label {
+            /* affects the label text, but not the input text itself */
+            color: white
         }
 
         .submit-button {
@@ -115,19 +166,15 @@
             transform: translateY(4px);
         }
 
-        .form-group label {
-            /* affects the label text, but not the input text itself */
-            color: white
-        }
     </style>
 </head>
 <!-- compensate for navbar and footer -->
 <br><br>
-<body style="font-family: 'Arial'">
+<body style="font-family: Arial, sans-serif">
     <div class="content-container">
         <div class="yellow-border-box">
             <h1 style="font-size: 36px"><b>Log In</b></h1>
-            <h3><u>Hurry!</u> Log in by <b><?php echo date("d/m/Y", strtotime("+1 week")); ?></b> to claim 50 free Roulette spins!</h3>
+            <h3><u>Hurry!</u> Log in by <b><?php echo date("d/m/Y", strtotime("+1 week")); ?></b> to claim 50 free gallons of milk!</h3>
 
             <?php if ($registered) { ?>
                 <!-- if the user was referred from the registration page, then say they succeeded and tell them to log in -->
@@ -136,18 +183,18 @@
 
             <?php if ($error !== "") { ?>
                 <!-- if user runs into an error, echo it -->
-                <p><?php echo $error; ?></p>
+                <p style="color: rgb(255, 136, 136); font-size: 20px">ERROR: <?php echo $error; ?></p>
             <?php } ?>
 
             <form method="POST" action="index.php">
                 <div style="font-size: 18px;" class="form-group">
                     <label>Username:</label><br>
                     <!-- inputs can use some styling, basic as-is -->
-                    <input type="text" name="username" placeholder=" John Doe" required>
+                    <input type="text" name="username" placeholder="John Doe" required>
                 </div>
                 <div style="font-size: 18px;" class="form-group">
                     <label>Email Address:</label><br>
-                    <input type="email" name="email" placeholder=" JohnDoe@email.com" required>
+                    <input type="email" name="email" placeholder="JohnDoe@email.com" required>
                 </div>
                 <div style="font-size: 18px;" class="form-group">
                     <label>Password:</label><br>
